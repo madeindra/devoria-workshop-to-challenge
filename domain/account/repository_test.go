bf0162aba4b14717ff2a7aed3340d0929fe74493
Made Indra <made.indra@protonmail.com>
2021-12-12 00:00:07 +0800

    complete repo test

--- domain/account/repository_test.go
+++ domain/account/repository_test.go
@@ -12,14 +12,17 @@ import (
 	"golang.org/x/net/context"
 )
 
+var accountPassword = "secret"
+var currentTime = time.Date(2021, 12, 12, 0, 0, 0, 0, &time.Location{})
+
 var account = &Account{
 	ID:             1,
 	FirstName:      "User",
 	Email:          "user@example.com",
 	LastName:       "08123456789",
-	Password:       nil,
-	CreatedAt:      time.Date(2021, 12, 12, 0, 0, 0, 0, &time.Location{}),
-	LastModifiedAt: nil,
+	Password:       &accountPassword,
+	CreatedAt:      currentTime,
+	LastModifiedAt: &currentTime,
 }
 
 func TestFindByID(t *testing.T) {
@@ -31,7 +34,7 @@ func TestFindByID(t *testing.T) {
 	query := fmt.Sprintf(`SELECT id, email, password, firstName, lastName, createdAt, lastModifiedAt FROM %s WHERE id = ?`, constant.TableAccount)
 	rows := sqlmock.NewRows([]string{"id", "email", "password", "firstName", "lastName", "createdAt", "lastModifiedAt"}).AddRow(account.ID, account.Email, account.Password, account.FirstName, account.LastName, account.CreatedAt, account.LastModifiedAt)
 
-	ctx := context.Background()
+	ctx := context.TODO()
 
 	mock.ExpectPrepare(query).ExpectQuery().WithArgs(account.ID).WillReturnRows(rows)
 
@@ -48,11 +51,114 @@ func TestFindByIDError(t *testing.T) {
 	query := fmt.Sprintf(`SELECT id, email, password, firstName, lastName, createdAt, lastModifiedAt FROM %s WHERE id = ?`, constant.TableAccount)
 	rows := sqlmock.NewRows([]string{"id", "email", "password", "firstName", "lastName", "createdAt", "lastModifiedAt"})
 
-	ctx := context.Background()
+	ctx := context.TODO()
 
 	mock.ExpectPrepare(query).ExpectQuery().WithArgs(account.ID).WillReturnRows(rows)
 
-	account, err := repo.FindByID(ctx, account.ID)
+	user, err := repo.FindByID(ctx, account.ID)
+	assert.Empty(t, user)
+	assert.Error(t, err)
+}
+
+func TestFindByEmail(t *testing.T) {
+	db, mock := mock.NewMock()
+	repo := NewAccountRepository(db, constant.TableAccount)
+
+	defer db.Close()
+
+	query := fmt.Sprintf(`SELECT id, email, password, firstName, lastName, createdAt, lastModifiedAt FROM %s WHERE email = ?`, constant.TableAccount)
+	rows := sqlmock.NewRows([]string{"id", "email", "password", "firstName", "lastName", "createdAt", "lastModifiedAt"}).AddRow(account.ID, account.Email, account.Password, account.FirstName, account.LastName, account.CreatedAt, account.LastModifiedAt)
+
+	ctx := context.TODO()
+
+	mock.ExpectPrepare(query).ExpectQuery().WithArgs(account.Email).WillReturnRows(rows)
+
+	user, err := repo.FindByEmail(ctx, account.Email)
+	assert.NotNil(t, user)
+	assert.NoError(t, err)
+}
+
+func TestFindByEmailError(t *testing.T) {
+	db, mock := mock.NewMock()
+	repo := NewAccountRepository(db, constant.TableAccount)
+	defer db.Close()
+
+	query := fmt.Sprintf(`SELECT id, email, password, firstName, lastName, createdAt, lastModifiedAt FROM %s WHERE id = ?`, constant.TableAccount)
+	rows := sqlmock.NewRows([]string{"id", "email", "password", "firstName", "lastName", "createdAt", "lastModifiedAt"})
+
+	ctx := context.TODO()
+
+	mock.ExpectPrepare(query).ExpectQuery().WithArgs(account.Email).WillReturnRows(rows)
+
+	account, err := repo.FindByEmail(ctx, account.Email)
 	assert.Empty(t, account)
+	assert.Error(t, err)
+}
+
+func TestCreate(t *testing.T) {
+	db, mock := mock.NewMock()
+	repo := NewAccountRepository(db, constant.TableAccount)
+
+	defer db.Close()
+
+	query := fmt.Sprintf(`INSERT INTO %s`, constant.TableAccount)
+
+	ctx := context.TODO()
+
+	mock.ExpectPrepare(query).ExpectExec().WithArgs(account.Email, account.Password, account.FirstName, account.LastName, account.CreatedAt).WillReturnResult(sqlmock.NewResult(1, 1))
+
+	ID, err := repo.Create(ctx, *account)
+	assert.Equal(t, int64(1), ID)
+	assert.NoError(t, err)
+}
+
+func TestCreateError(t *testing.T) {
+	db, mock := mock.NewMock()
+	repo := NewAccountRepository(db, constant.TableAccount)
+
+	defer db.Close()
+
+	query := fmt.Sprintf(`INSERT INTO %s`, constant.TableAccount)
+
+	ctx := context.TODO()
+
+	mock.ExpectPrepare(query).ExpectExec().WithArgs(account.Email, account.Password, account.FirstName, account.LastName, account.CreatedAt).WillReturnResult(sqlmock.NewResult(0, 0))
+
+	ID, err := repo.Create(ctx, *account)
+	assert.Equal(t, int64(0), ID)
+	assert.NoError(t, err)
+}
+
+func TestUpdate(t *testing.T) {
+	db, mock := mock.NewMock()
+	repo := NewAccountRepository(db, constant.TableAccount)
+
+	defer db.Close()
+
+	query := fmt.Sprintf(`UPDATE %s SET`, constant.TableAccount)
+
+	ctx := context.TODO()
+
+	mock.ExpectPrepare(query).ExpectExec().WithArgs(account.Password, account.FirstName, account.LastName, account.LastModifiedAt).WillReturnResult(sqlmock.NewResult(0, 1))
+
+	err := repo.Update(ctx, account.ID, *account)
+
+	assert.NoError(t, err)
+}
+
+func TestUpdateError(t *testing.T) {
+	db, mock := mock.NewMock()
+	repo := NewAccountRepository(db, constant.TableAccount)
+
+	defer db.Close()
+
+	query := fmt.Sprintf(`UPDATE %s SET`, constant.TableAccount)
+
+	ctx := context.TODO()
+
+	mock.ExpectPrepare(query).ExpectExec().WithArgs(account.Password, account.FirstName, account.LastName, account.LastModifiedAt).WillReturnResult(sqlmock.NewResult(0, 0))
+
+	err := repo.Update(ctx, account.ID, *account)
+
 	assert.Error(t, err)
 }
